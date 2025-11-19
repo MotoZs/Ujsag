778b21cff3b64551c0ffb947485efd5239c04346
b1csimar <b1csimar@vasvari.org>
2025-11-19 08:29:19 +0100

    Common_kesz

---
+++ Common/ArticleDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace Common
+{
+    public record ArticleDto ()
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public AuthorDto Author { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
